a9ca903e37c7bb20e4e48a4ba5e8c09147dea99c
Preety Kumari <preetysingh46231@gmail.com>
2024-04-19 23:31:12 +0530

    Get and post request donein Todos route

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ app.use(express.json());
 const cors = require("cors");
 const { connection } = require("./Backend/config/db");
 const { userRouter } = require("./Backend/routes/users.route");
+const { todosRouter } = require("./Backend/routes/todos.route");
+
 const port = Number(process.env.PORT) || 8080;
+
 app.use("/users", userRouter);
+app.use("/todos", todosRouter);
+
 app.get("/", (req,res)=>{
     res.send("Home");
 })
