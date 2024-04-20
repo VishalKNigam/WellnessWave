a971a506c02cf097a930ab8378386852ca3bc664
Vishal Kumar Nigam <115461865+VishalKNigam@users.noreply.github.com>
2024-04-20 21:33:17 +0530

    update and delete functionanlity done in UserRoute

--- Backend/routes/users.route.js
+++ Backend/routes/users.route.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { UserModel } = require("../models/users.model");
 const userRouter = express.Router();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 userRouter.get("/", async (req, res) => {
     const { page, limit } = req.query;
     try {
@@ -19,15 +21,52 @@ userRouter.get("/", async (req, res) => {
 });
 // User Registeration
 
-userRouter.post("/register", async(req, res)=>{
+userRouter.post("/register", async (req, res) => {
     try {
         const user = new UserModel(req.body);
         await user.save();
-        res.send({msg:`Welcome, ${req.body.name} Your Registration is Successful!` });
+        res.send({ msg: `Welcome, ${req.body.name} Your Registration is Successful!` });
     } catch (error) {
         res.send({ "error": error.message })
     }
-})
+});
+userRouter.post("/login", async (req, res) => {
+    const { email } = req.body;
+    const user = await UserModel.findOne({ email });
+    try {
+        const token = jwt.sign({ userId: user._id, role: user.role },
+            process.env.jwt_secret_key, { expiresIn: "24h" });
+        res.send({
+            msg: `Welcome!, ${user.name} Your Login is Successful.`, token: token, userId: user._id,
+            role: user.role, name: user.name
+
+        });
+    } catch (error) {
+        res.send({ err: error.message })
+
+    }
+});
+userRouter.patch("/update",async (req,res)=>{
+    const {userId,name} = req.body;
+    try {
+        await UserModel.findByIdAndUpdate(userId,req.body);
+        res.send({msg:`${name}'s Details are Updated Successfully.`})
+    } catch (error) {
+        res.send({err:error.message})
+    }
+});
+
+userRouter.delete("/delete/:id",async (req,res)=>{
+    const {id} = req.params;
+    let user = await UserModel.findById(id);
+    try {
+        let DeleteUser = await UserModel.findByIdAndDelete(id);
+        // console.log(DeleteUser);
+        res.send({msg:`${user.name}'s Account is Deleted Successfully.`,Deleted_User:DeleteUser})
+    } catch (error) {
+        res.send({err:error.message})
+    }
+});
 
 module.exports = { userRouter };
 
